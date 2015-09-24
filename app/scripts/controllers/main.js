316916513d01d29b891d3f99670529aacd018344
Alain Chautard <achautard@gmail.com>
2015-09-23 17:46:52 -0700

    Step 2 - Get weather data through AJAX & add a remove location button

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the angularWeatherApp
  */
 angular.module('angularWeatherApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($http) {
     this.locations = [ ];
+    var locations = this.locations;
 
-      this.addLocation = function(zipcode){
-        this.locations.push(zipcode);
+    this.addLocation = function(zipcode){
+      $http.get("http://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&units=imperial").
+        then(function(response) {
+          var newLocation = {zip: zipcode, weather: response.data.weather[0], temp: response.data.main, name: response.data.name};
+          locations.push(newLocation);
+        });
+    }
+
+    this.removeLocation = function(zipcode){
+      angular.forEach(locations, function(value, index) {
+        if (value.zip === zipcode)
+          locations.splice(index, 1);
       }
+      );
+    }
   });
